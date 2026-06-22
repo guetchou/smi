c7f4780a826f86f83f861eb2edb523c857020682
guetchou <140221073+guetchou@users.noreply.github.com>
2026-06-22 23:51:01 +0100

    feat(hr): load department functions interface

--- frontend/js/modules/org-departments.js
+++ frontend/js/modules/org-departments.js
@@ -7,6 +7,7 @@
     '/js/modules/org-integrity-ui.js',
     '/js/modules/org-mutation-workflow-ui.js',
     '/js/modules/agent-organization-workflow-lock.js',
+    '/js/modules/org-department-functions-ui.js',
   ];
 
   scripts.reduce((chain, src) => chain.then(() => new Promise((resolve, reject) => {
