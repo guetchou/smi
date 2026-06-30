95a92a81e98418fa0e611802b1427181d56baa5d
Gess GALOYI <gess.galoyi@gmail.com>
2026-06-30 14:15:39 +0100

    test(hr): add configurable leave business calendar

---
+++ backend/migrations/039_leave_calendar_parameters.sql
@@ -0,0 +1,10 @@
+INSERT IGNORE INTO parametres (cle, valeur) VALUES
+  ('conges_calcul_annuel', 'ouvres'),
+  ('conges_calcul_maladie', 'calendaires'),
+  ('conges_calcul_maternite', 'calendaires'),
+  ('conges_calcul_paternite', 'calendaires'),
+  ('conges_calcul_sans_solde', 'ouvres'),
+  ('conges_calcul_autre', 'ouvres'),
+  ('conges_weekend', '6,0'),
+  ('conges_timezone', 'Africa/Brazzaville'),
+  ('conges_jours_feries', '');
